bdade73b84e5fac5caf08ee501fb56c108f5f708
AaronFrans <azdefatrhtws@gmail.com>
2020-11-01 10:33:09 +0100

    RepositoryPattern done

---
+++ DomainLayer/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using DomainLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer
+{
+    interface IUnitOfWork : IDisposable
+    {
+        public IClientRepository Clients {get;}
+        int Complete();
+    }
+}
